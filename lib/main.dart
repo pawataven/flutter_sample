f92a2a5c9e4f9d21497843e7b91c88007e7712fd
pawataven <pawatwantrong@gmail.com>
2024-12-08 17:03:15 +0700

    randompage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_2/random.dart';
 // import 'package:flutter_2/scroll.dart';
 // import 'package:flutter_2/my_container.dart';
 import 'package:flutter_2/work_widget.dart';
@@ -6,6 +7,6 @@ import 'package:flutter_2/work_widget.dart';
 void main() {
   runApp( const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: WyWidgetWork(),
+    home: Randomnumber(),
   ));
 }
