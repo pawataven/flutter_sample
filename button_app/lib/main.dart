694f7f808bd72536fb3023a06f14f079b89273df
pawataven <pawatwantrong@gmail.com>
2025-01-06 11:15:39 +0700

    2

--- button_app/lib/main.dart
+++ button_app/lib/main.dart
@@ -10,11 +10,69 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+      );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _navItemIndex = 0; //ลำดัยที่ของปุ่มที่เลือก
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigaionBar'),
+        centerTitle: true,
+      ),
+      body: null,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurple, //สีพื้นหลัง
+        fixedColor: Colors.yellow, //สีปุ่มที่ถูกเลือก
+
+        showSelectedLabels: true, //แสดงข้อความบนปุ่มที่ถูกเลือก
+        showUnselectedLabels: true, //แสดงข้อความบนปุ่มที่ไม่ถูกเลือก
+        currentIndex: _navItemIndex,
+        items: btnNavItem(),
+        onTap: (index) {
+          setState(() {
+            _navItemIndex = index;
+          });
+        },
       ),
     );
+  }
+
+  List<BottomNavigationBarItem> btnNavItem() {
+    var iconItem = [Icons.home, Icons.shopping_cart, Icons.person, Icons.help];
+    var labelItem = ['Home', 'Product', 'Member', 'Hele'];
+    var bgColorItem = [
+      Colors.deepPurple,
+      Colors.indigo,
+      Colors.teal,
+      Colors.brown
+    ];
+
+    List<BottomNavigationBarItem> mybtn;
+
+    BottomNavigationBarItem createItem(index) {
+      return BottomNavigationBarItem(
+          icon: Icon(iconItem[index]),
+          label: labelItem[index],
+          backgroundColor: bgColorItem[index]);
+    }
+
+    mybtn = List.generate(iconItem.length, createItem);
+
+    return mybtn;
   }
 }
